2acd7f09b372f1a58696f4bb2ebe7609d4722e0e
samuelsp <samuelsp@media.mit.edu>
2017-07-18 07:16:32 -0700

    added rooster to nettalk

--- TapGameController/TapGameUtils/GlobalSettings.py
+++ TapGameController/TapGameUtils/GlobalSettings.py
@@ -3,7 +3,7 @@ This is a module that exports certain global runtime settings
 """
 # -*- coding: utf-8 -*-
 
-USE_ROS = False
+USE_ROS = True
 USE_TEGA = True # if False, we are using Jibo
 
 class TapGameLog(): # pylint: disable=too-few-public-methods

22ba24363befc9e21c48f592f9b8f5fe479b63e6
samuelsp <samuelsp@media.mit.edu>
2017-09-16 20:40:23 -0400

    moved files around, added new makefile scripts for generating covariance matrix

--- GameUtils/PronunciationUtils/PronunciationUtils.py
+++ GameUtils/PronunciationUtils/PronunciationUtils.py
@@ -9,18 +9,20 @@ import pronouncing
 import csv
 import os
 import pandas as pd
-from weighted_levenshtein.clev import levenshtein as lev
 import numpy as np
 
-SCORE_THRESHOLD = 70 # Score threshold we use to determine whether a word was passed or not
-					 # by SpeechAce
-
-PHONEME_SUB_COST_PATH = '/GameUtils/phoneme_sub_costs'
-
-class PronunciationHandler:
+class PronunciationUtils:
 	"""
 	This class contains functions that handles pronunciation results obtained from SpeechACE
 	"""
+
+	SCORE_THRESHOLD = 70  # Score threshold we use to determine whether a word was passed or not
+	# by SpeechAce
+
+	PHONEME_SUB_COST_PATH = '/GameUtils/PronunciationUtils/data/phoneme_subsitution_costs'
+	WEIGHTED_LEV_DISTANCE_PATH = '/GameUtils/PronunciationUtils/data/weighted_lev_distances'
+	GLOVE_DISTANCE_PATH = '/GameUtils/PronunciationUtils/data/glove_distances'
+	COVARIANCE_PATH = '/GameUtils/PronunciationUtils/data/covariance_matrix'
 
 	def __init__(self):
 
@@ -29,10 +31,6 @@ class PronunciationHandler:
 
 		# load nettalk.data dictionary, which is used to alignment word graphemes and phonemes
 		self.load_nettalk_dataset()
-
-		# read the phoneme substitution costs from disk
-		self.substitute_costs = np.load(os.getcwd() + PHONEME_SUB_COST_PATH + '.npy')
-
 
 		self.current_word = None
 		self.pho_results = None
@@ -125,7 +123,7 @@ class PronunciationHandler:
 
 		# dictionary to store arpabet mapping
 		self.arpabet_map = dict()
-		with open(dir_path + '/arpabet_mapping.csv', 'r') as csvfile:
+		with open(dir_path + '/data/arpabet_mapping.csv', 'r') as csvfile:
 			spamreader = csv.reader(csvfile, delimiter=',')
 			for row in spamreader:
 				# row[1] is CMU arpabet, row[0] is modified version of arpabet for NETTalk database
@@ -141,7 +139,7 @@ https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectio
 		dir_path = os.path.dirname(os.path.realpath(__file__))
 
 		self.nettalk_dataset = dict()
-		with open(dir_path + '/nettalk.data', 'r') as csvfile:
+		with open(dir_path + '/data/nettalk.data', 'r') as csvfile:
 			spamreader = csv.reader(csvfile, delimiter='\t')
 			for row in spamreader:
 				# row[0]: word. row[1]: phonetic transcription
@@ -211,97 +209,3 @@ https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectio
 		#print(word + " become " + output)
 		return output
 
-	def build_substitution_score_matrix(self):		
-
-		# read weighted phonemic similarity matrix,
-		# downloaded from https://github.com/benhixon/benhixon.github.com/blob/master/wpsm.txt
-		wpsm_filepath = '/GameUtils/wpsm.csv'
-		# load the matrix csv file into a dataframe
-		df = pd.read_csv(os.getcwd() + wpsm_filepath, sep=',', header=0, index_col=0)
-
-		arpabet_phonemes = df.keys()
-		print(arpabet_phonemes)
-		print(len(arpabet_phonemes))
-
-		# check whether arpabet map is empty. if it is, then load the map
-		if not self.arpabet_map:
-			print("load mapping")
-			self.load_arpabet_mapping()
-			print(self.arpabet_map)
-
-		substitute_costs = np.ones((128, 128), dtype=np.float64)  # make a 2D array of 1's. ASCII table
-		# update the original substitution matrix
-		lowest = 100
-		lowkey1 = None
-		lowkey2 = None
-
-		highest = -100
-		lowkey1 = None
-		lowkey2 = None
-
-		# calculate min/max values of non-diagonal wpsm scores
-		for key1 in arpabet_phonemes:
-			for key2 in arpabet_phonemes:
-				if not key1 == key2:
-					if (-1 * df[key1][key2]) < lowest:
-						lowkey1 = key1
-						lowkey2 = key2
-						lowest = -1 * df[key1][key2] #"P/B"
-
-					if (-1 * df[key1][key2]) > highest:
-						highkey1 = key1
-						highkey2 = key2
-						highest = -1 * df[key1][key2]
-
-		print("lowest score was ")
-		print(lowest)
-		print(df[lowkey1][lowkey2])
-
-		print("highest score was ")
-		print(highest)
-		print(df[highkey1][highkey2])	
-
-		range = highest-lowest
-		print(self.normalize_wpsm_cost(highest, highest, lowest))
-		print(self.normalize_wpsm_cost(lowest, highest, lowest))					
-
-		for key1 in arpabet_phonemes:
-			for key2 in arpabet_phonemes:
-				nkey1 = self.arpabet_map[key1]
-				nkey2 = self.arpabet_map[key2]
-
-				if (nkey1 == nkey2):
-					substitute_costs[ord(nkey1), ord(nkey2)] = 0.0 # use zero substitution cost for diagonal entries
-				else:
-					raw_cost = -1 *df[key1][key2]
-					substitute_costs[ord(nkey1), ord(nkey2)] = self.normalize_wpsm_cost(raw_cost, highest, lowest)
-
-		np.save(os.getcwd() + PHONEME_SUB_COST_PATH, substitute_costs)
-		np.savetxt(os.getcwd() + PHONEME_SUB_COST_PATH + '.txt', substitute_costs)
-
-	def normalize_wpsm_cost(self, raw_cost, highest, lowest):
-		"""
-		converts a raw_cost into the range (0,1), given the highest and lowest wpsm costs
-		See: https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
-		"""
-		#print(raw_cost)
-		#print("TURNED INTO")
-		scaled_cost = ((1 - 0)*(raw_cost - lowest)/(highest - lowest))
-		#print(str(scaled_cost) + '\n')
-		return scaled_cost
-
-
-
-	def measure_weighted_levenshtein_distance(self, word1, word2):
-		# import weighted levenshtein library. if clev is missing, change the __init__.py in the weighted_levenshtein lib to add clev.so path to sys.
-		# /anaconda3/lib/python3.6/site-packages/weighted_levenshtein
-		# delete "from clev import *" in __init__.py
-
-		result = lev(self.get_phonetic_similarity_rep(word1).encode(),
-		 			 self.get_phonetic_similarity_rep(word2).encode(),
-		 			 substitute_costs=self.substitute_costs)	
-
-		# normalize the levenshtein score by taking max(str1,str2)
-		denominator = max(len(word1), len(word2))
-		normalized_score = result / float(denominator)
-		return normalized_score
--- TapGameController/StudentModel.py
+++ TapGameController/StudentModel.py
@@ -11,12 +11,11 @@ import operator
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
-import spacy
+import os
 
-from GameUtils.GlobalSettings import USE_SPACY
 from GameUtils.GlobalSettings import DO_ACTIVE_LEARNING
 from GameUtils.Curriculum import Curriculum
-from GameUtils.PronunciationUtils import PronunciationHandler
+from GameUtils.PronunciationUtils.PronunciationUtils import PronunciationUtils
 
 from .AgentModel import ActionSpace
 
@@ -33,15 +32,15 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
 
     def __init__(self):
 
-        if USE_SPACY:
-            self.nlp = spacy.load('en') #sets spacy up with the english language model
-
         #fancy python one-liner to read all string attributes off of a class
         self.curriculum = [p for p in dir(Curriculum)
                            if isinstance(getattr(Curriculum, p), str)
                            and not p.startswith('__')]
 
-        self.pronunciationHandler = PronunciationHandler()                           
+        self.pronunciationUtils = PronunciationUtils()
+
+        self.loaded_covariance_matrix = np.load(
+            os.getcwd() + self.PronunciationUtils.COVARIANCE_PATH + '.npy')
 
         # these parameters govern the assumed Gaussian noise added to the child's recorded
         # pronunciation assessment
@@ -78,7 +77,7 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
         # we only care about evaluating the GP at this finite collection of points
         x_test = self.curriculum
 
-        K_ss = self.concept_net_kernel(x_test, x_test) #the Kernel Matrix for the words
+        K_ss = self.psdm_kernel(x_test, x_test) #the Kernel Matrix for the words
 
         # Get cholesky decomposition (square root) of the
         # covariance matrix
@@ -112,8 +111,8 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
         print(self.Y_train)
         print(self.means)
         print(self.variances)
-        K = self.concept_net_kernel(self.X_train, self.X_train)
-        K_ss = self.concept_net_kernel(Xtest, Xtest)
+        K = self.psdm_kernel(self.X_train, self.X_train)
+        K_ss = self.psdm_kernel(Xtest, Xtest)
 
         L = np.linalg.cholesky(K + 0.00005 * np.eye(len(self.X_train)) +
                                ((self.noise_sigma ** 2) * np.eye(len(self.X_train))))
@@ -123,7 +122,7 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
         a = np.linalg.solve(L.T, L_y)
 
         # Compute the mean at our test points.
-        K_s = self.concept_net_kernel(self.X_train, Xtest)
+        K_s = self.psdm_kernel(self.X_train, Xtest)
         mu = self.get_mean(Xtest) + np.dot(K_s.T, a).reshape(len(self.curriculum), )
         #mu = np.dot(K_s.T, a).reshape(len(self.curriculum), ) #zero mean function version
         v = np.linalg.solve(L, K_s)
@@ -193,9 +192,9 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
         return np.exp(-.5 * (1 / length_scale) * sqdist)
 
 
-    def concept_net_kernel(self, word_set_a, word_set_b):
+    def psdm_kernel(self, word_set_a, word_set_b):
         """
-        Implements a conceptnet based covariance matrix
+        Calculates word matrix covariance via a phonetic-semantic distance metric
         """
         #k = np.empty((len(word_set_a), len(word_set_b),))
 
@@ -211,32 +210,17 @@ class StudentModel(): # pylint: disable=invalid-name,consider-using-enumerate,to
 
         return k
 
-    def get_word_cov(self, word_a, word_b):
+    def get_word_cov(self, word1, word2):
         """
-        Currently, just implements a letter counting distance metric.
-        Will eventually incorporate conceptnet and other phonetic metrics
+        Returns specific values from the global covariance matrix
         """
 
-        if word_a == word_b:
-            return 1
+        index1 = self.curriculum.index(word1)
+        index2 = self.curriculum.index(word2)
 
-        score = 0
-        for letter_a in word_a:
-            if letter_a in word_b:
-                score += 1
-
-        for letter_b in word_b:
-            if letter_b in word_a:
-                score += 1
-
-        ratio = (score / (len(word_a) + len(word_b)))
-        #print("letter counting similarity between " + word_a + " and " + word_b)
-        #print(round(ratio, 2))
-        
-        #print("inverse weighted lev distance between " + word_a + " and " + word_b)
-        #print(1 - self.pronunciationHandler.measure_weighted_levenshtein_distance(word_a,word_b))
-        #return(1 - self.pronunciationHandler.measure_weighted_levenshtein_distance(word_a,word_b))
-        return (round(ratio, 2))
+        print("covariance between " + word1 + " and " + word2)
+        print(round(self.loaded_covariance_matrix[index1][index2], 3))
+        return round(self.loaded_covariance_matrix[index1][index2], 3)
 
     def get_mean(self, X):
         """

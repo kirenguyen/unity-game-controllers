e45e8a0a04a793bf4a597c194fd56b04e95f2932
Sam Spaulding <you@example.com>
2018-05-23 12:09:21 -0700

    cleaned up non-essential AudioRecorder calls

--- iSpyGameController/iSpyGameFSM.py
+++ iSpyGameController/iSpyGameFSM.py
@@ -27,7 +27,7 @@ from GameUtils.PronunciationUtils.PronunciationUtils import PronunciationUtils
 from .ROSNodeMgr import ROSNodeMgr
 from .iSpyDataTracking import iSpyDataTracking
 #from .StudentModel import StudentModel
-from .RobotBehaviorList.RobotBehaviorList import RobotBehaviors 
+from .RobotBehaviorList.RobotBehaviorList import RobotBehaviors
 from .RobotBehaviorList.RobotBehaviorList import RobotRoles
 from .RobotBehaviorList.RobotBehaviorList import RobotRolesBehaviorsMap
 
@@ -39,7 +39,7 @@ from .GameModeFSMs import AlwaysMissionModeFSM,CompleteModeFSM,AlwaysExploreMode
 
 from multiprocessing import Process
 
-import datetime 
+import datetime
 # from StudentModel import StudentModel
 
 if GlobalSettings.USE_ROS:
@@ -128,8 +128,8 @@ class iSpyGameFSM: # pylint: disable=no-member
 		# start a thread to check the game update
 		#self.t = threading.Thread(target=self.update)
 		#self.t.start()
-		
-		
+
+
 	def update(self):
 		while self.kill_received == False:
 			if self.FSM.state != gs.MISSION_MODE:
@@ -142,12 +142,12 @@ class iSpyGameFSM: # pylint: disable=no-member
 
 	def _reach_max_task_time(self): # if the condition is in "fixed novice": set a max elapsed time
 			if self.interaction.subj_cond != "novice": return
-			
+
 			max_elapsed_time = datetime.timedelta(seconds=4.5*60) # 5 mins
 			max_elapsed_time2 = datetime.timedelta(seconds=6.5*60) # 5 mins
 			if datetime.datetime.now() - self.task_controller.get_task_time()['start'] > max_elapsed_time:
 				if self.task_controller.num_finished_words <= 2: self.task_controller.reset_for_new_task()
-			
+
 			if datetime.datetime.now() - self.task_controller.get_task_time()['start'] > max_elapsed_time2:
 				if self.task_controller.num_finished_words <= 3: self.task_controller.reset_for_new_task()
 
@@ -159,10 +159,10 @@ class iSpyGameFSM: # pylint: disable=no-member
 		def check_task_completion():
 			if not self.task_controller.task_in_progress:
 				# let the game knows the task is completed
-				self.ros_node_mgr.send_ispy_cmd(TASK_COMPLETED)	
+				self.ros_node_mgr.send_ispy_cmd(TASK_COMPLETED)
 				self.current_task_index += 1
-				if self.current_task_index != 0: 
-					action_number = self.current_task_index 
+				if self.current_task_index != 0:
+					action_number = self.current_task_index
 					self.interaction.start_task_end_behavior(action_number)
 
 		#print("State Transition: "+transition_msg.data)
@@ -183,42 +183,42 @@ class iSpyGameFSM: # pylint: disable=no-member
 				self.ros_node_mgr.send_ispy_cmd(34, self.session_number) #SET_GAME_SCNE = 34
 				print("CONNECT_BUTTON_PRESSED : "+self.session_number)
 				self.ros_node_mgr.send_robot_cmd(RobotBehaviors.ROBOT_HAPPY_DANCE)
-				
+
 			elif transition_msg.data == gs.Triggers.HINT_BUTTON_PRESSED:
 				self.interaction.numHintButtonPressedForTask += 1
 
 
 			elif transition_msg.data == gs.Triggers.OBJECT_CLICKED:
 				pass
-		
-				
+
+
 			elif transition_msg.data == gs.Triggers.NONTARGET_OBJECT_COLLECTED or transition_msg.data == gs.Triggers.TARGET_OBJECT_COLLECTED:
 				self._reach_max_task_time()
 				check_task_completion()
 				self.interaction.turn_taking()
-				
+
 			# elif transition_msg.data == gs.Triggers.TARGET_OBJECT_COLLECTED:
 			# 	self._reach_max_task_time()
 			# 	check_task_completion()
 			# 	self.interaction.turn_taking()
-				
+
 			elif transition_msg.data == gs.Triggers.PRONUNCIATION_PANEL_CLOSED:
 				if self.interaction.state == ris.CHILD_TURN: # when a new turn is child's, then start tracking the child's interaction
-					t = threading.Timer(3.0, self.interaction.start_tracking_child_interaction).start() # checking for timeout 
-			
+					t = threading.Timer(3.0, self.interaction.start_tracking_child_interaction).start() # checking for timeout
+
 			elif transition_msg.data == gs.Triggers.SCREEN_MOVED:
 				self.interaction.stop_tracking_child_interaction()
 
 			# If the message is in gs.Triggers, then allow the trigger
 			if transition_msg.data != gs.Triggers.SCREEN_MOVED:
 				self.FSM.start_trigger(transition_msg.data)
-				
-			
+
+
 
 	#################################################################################################
 
 	def on_ispy_log_received(self, log_msg):
-		
+
 		# If the the message was "messageReceived", that means that the publishing loop can stop
 		if log_msg.data == "messageReceived":
 			self.ros_node_mgr.message_received = True
@@ -231,7 +231,7 @@ class iSpyGameFSM: # pylint: disable=no-member
 		"""
 
 
-		if self.FSM.state == gs.EXPLORATION_MODE or self.FSM.state == gs.WORD_DISPLAY: 
+		if self.FSM.state == gs.EXPLORATION_MODE or self.FSM.state == gs.WORD_DISPLAY:
 		# if the game is still in explore mode
 			return
 
@@ -239,16 +239,19 @@ class iSpyGameFSM: # pylint: disable=no-member
 		time.sleep(0.1) # wait for on_ispy_state_info_received() to finish and FSM to transition first
 		#self.interaction.stop_tracking_child_interaction() # start tracking the elapsed time of child's lack of tablet interaction
 
-		
+
 		self.iSpyDataTracking.on_ispy_action_received(ispy_action_msg)
 
 		def speakingStage(stage):
 			if stage == "speakingStart":
-				self.recorder.start_recording(self.origText, RECORD_TIME_MS, self.interaction.state) #TODO: Update 'test' to actual word
+				self.recorder.has_recorded += 1
+			# 	self.recorder.start_recording(self.origText, RECORD_TIME_MS, self.interaction.state)
+		# #TODO: Update 'test' to actual word
 			elif stage == "speakingEnd":
-				self.recorder.stop_recording()
-	
-		
+				self.recorder.has_recorded += 1
+			# 	self.recorder.stop_recording()
+
+
 		def msg_evaluator(ispy_action_msg):
 			"""Calls the respective functions for each part of the action msg
 			"""
@@ -265,7 +268,7 @@ class iSpyGameFSM: # pylint: disable=no-member
 
 			#Initializes a new audio recorder object if one hasn't been created
 			if self.recorder == None: self.recorder = AudioRecorder()
-	
+
 			speakingStage(ispy_action_msg.speakingStage)
 
 		print("\n")
@@ -279,7 +282,7 @@ class iSpyGameFSM: # pylint: disable=no-member
 
 		if self.interaction.state == ris.CHILD_TURN or self.interaction.state == ris.ROBOT_TURN+'_'+ris.CHILD_HELP:
 			pass
-			# if self.FSM.state == gs.MISSION_MODE: 
+			# if self.FSM.state == gs.MISSION_MODE:
 			# 	threading.Timer(0.5, self.interaction.start_tracking_child_interaction).start() # start tracking the elapsed time of child's lack of tablet interaction
 
 		self.isDragging = ispy_action_msg.isDragging
@@ -289,8 +292,8 @@ class iSpyGameFSM: # pylint: disable=no-member
 		self.isScalingDown = ispy_action_msg.isScalingDown
 
 		self.interaction._ros_publish_data("","", True)
-		
-			
+
+
 	def _speechace_analysis(self):
 		'''
 		speech ace analysis
@@ -303,27 +306,31 @@ class iSpyGameFSM: # pylint: disable=no-member
 
 			print("clicked object is: "+self.origText)
 
-			self.correct_obj = self.task_controller.isTarget(self.origText) 
-			
-			
-			if not self.recorder.valid_recording:
-				letters = list(self.origText)
-				passed = ['1'] * len(letters)
-				print ("NO, RECORDING SO YOU AUTOMATICALLY PASS")
+			self.correct_obj = self.task_controller.isTarget(self.origText)
 
-			else:
-				audioFile = self.recorder.WAV_OUTPUT_FILENAME_PREFIX + self.origText + '.wav'
-				word_score_list = self.recorder.speechace(audioFile)
+			letters = list(self.origText)
+			passed = ['1'] * len(letters)
+			print("NO RECORDING SO YOU AUTOMATICALLY PASS")
 
-				if word_score_list:
-					for word in word_score_list:
-						letters, passed = self.results_handler.process_speechace_word_results(word)
-					
-				else:
-					letters = list(self.origText)
-					passed = ['1'] * len(letters)
-					print ("NO, RECORDING SO YOU AUTOMATICALLY PASS")
-				
+			# TODO: Delete if we are not planning on using SpeechAce as real-time source of game info
+			# if not self.recorder.valid_recording:
+			# 	letters = list(self.origText)
+			# 	passed = ['1'] * len(letters)
+			# 	print ("NO RECORDING SO YOU AUTOMATICALLY PASS")
+
+			# else:AIzaSyDK8tFh08NPXRotdeOmSuTd8hzczxms7nY
+			# 	audioFile = self.recorder.WAV_OUTPUT_FILENAME_PREFIX + self.origText + '.wav'
+			# 	word_score_list = self.recorder.speechace(audioFile)
+			#
+			# 	if word_score_list:
+			# 		for word in word_score_list:
+			# 			letters, passed = self.results_handler.process_speechace_word_results(word)
+			#
+			# 	else:
+			# 		letters = list(self.origText)
+			# 		passed = ['1'] * len(letters)
+			# 		print ("NO, RECORDING SO YOU AUTOMATICALLY PASS")
+
 			results_params = {}
 			results_params["letters"] = letters
 			results_params["passed"] = passed
@@ -340,32 +347,32 @@ class iSpyGameFSM: # pylint: disable=no-member
 					self.task_controller.update_target_list(self.origText)
 					self.origText = ""
 
-			
+
 			#print(results_params)
 			self.ros_node_mgr.send_ispy_cmd(SEND_PRONOUNCIATION_ACCURACY_TO_UNITY, results_params)
 			self.recorder.has_recorded = 0
 
 
-	
-	
+
+
 	def _run_game_task(self):
 		# When entering mission mode from exploration mode, get a random task
 		# and send it to Unity
 
 		if self.task_controller.task_in_progress == False:
-		
+
 			task = self.task_controller.get_next_task()
 
 			# If there are no more available quests, you won the game
 			if task == None:
-			
+
 				self.ros_node_mgr.send_ispy_cmd(GAME_FINISHED)
 				self.interaction.child_states.done()
 			else:
 				self.ros_node_mgr.send_ispy_cmd(SEND_TASKS_TO_UNITY, task)
 				self.interaction.reset_turn_taking()
 				self.interaction.get_robot_general_response()
-				
+
 				t = threading.Timer(3.0,self.interaction.start_tracking_child_interaction).start()
 				threading.Timer(10.0, self.interaction.on_child_max_elapsed_time).start()
 
